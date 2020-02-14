d4dfd16fe1fe9c3cca1c9dd8e31c5db44d6fcb3a
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-02-14 11:08:22 +0100

    menu corregido

--- Aranda/MenúCartas.py
+++ Aranda/MenúCartas.py
@@ -1,5 +1,6 @@
-a=True
+a = True
 while a != False:
+    print("MENU PRINCIPAL")
     print("1) Cargar cartas")
     print("2) Cargar cartas Enemigo")
     print("3) Cargar mazo aleatorio")
@@ -13,31 +14,37 @@ while a != False:
     print("11) Luchar Jugador VS Jugador")
     print("12) Luchar Jugador VS Bot (Arcade)")
     print("13) Luchar Jugador VS Bot (liga)")
-    opc=int(input("Que opcion quieres utilizar?"))
+    try:
+        opc = int(input("Que opcion quieres utilizar? "))
+    except ValueError:
+        print("Opcion invalida, introduce una de las opciones anterior")
+        opc = int(input("Que opcion quieres utilizar? "))
 
     if opc == 1:
         print("Estas en la opción 1")
-    if opc == 2:
+    elif opc == 2:
         print("Estas en la opción 2")
-    if opc == 3:
+    elif opc == 3:
         print("Estas en la opción 3")
-    if opc == 4:
+    elif opc == 4:
         print("Estas en la opción 4")
-    if opc == 5:
+    elif opc == 5:
         print("Estas en la opción 5")
-    if opc == 6:
+    elif opc == 6:
         print("Estas en la opción 6")
-    if opc == 7:
+    elif opc == 7:
         print("Estas en la opción 7")
-    if opc == 8:
+    elif opc == 8:
         print("Estas en la opción 8")
-    if opc == 9:
+    elif opc == 9:
         print("Estas en la opción 9")
-    if opc == 10:
+    elif opc == 10:
         print("Estas en la opción 10")
-    if opc == 11:
+    elif opc == 11:
         print("Estas en la opción 11")
-    if opc == 12:
+    elif opc == 12:
         print("Estas en la opción 12")
-    if opc == 13:
+    elif opc == 13:
         print("Estas en la opción 13")
+    else:
+        print("Opcion invalida \n")
