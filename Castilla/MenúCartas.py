2c1dcf9cd12e7038f2040d89eaef8030dfffda9a
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-02-17 11:25:24 +0100

    modificaciones en el menu

--- Castilla/MenúCartas.py
+++ Castilla/MenúCartas.py
@@ -1,9 +1,32 @@
 from xml.dom import minidom
 listacartas = []
-def menu():
-    print("MENU PRINCIPAL")
+
+def cargarCartas(listacartas):
+    fichero = minidom.parse("./XML/Cartas.xml")
+    cartas = fichero.getElementsByTagName("card")
+
+    for carta in cartas:
+        listacarta = []
+        tipo = carta.getAttribute("type")
+        nombre = carta.getElementsByTagName("name")[0]
+        descripcion = carta.getElementsByTagName("description")[0]
+        ataque = carta.getElementsByTagName("attack")[0]
+        defensa = carta.getElementsByTagName("defense")[0]
+        invocacion = carta.getAttribute("summonPoints")
+        listacarta.append(tipo)
+        listacarta.append(nombre.firstChild.data.lstrip().rstrip())
+        listacarta.append(descripcion.firstChild.data.lstrip().rstrip())
+        listacarta.append(ataque.firstChild.data.lstrip().rstrip())
+        listacarta.append(defensa.firstChild.data.lstrip().rstrip())
+        listacarta.append(invocacion)
+        listacartas.append(listacarta)
+    return listacartas
+
+a = True
+while a != False:
     print("1) Cargar cartas")
     print("2) Cargar cartas Enemigo")
+    '''
     print("3) Cargar mazo aleatorio")
     print("4) Cargar mazo ofensivo")
     print("5) Cargar mazo defensivo")
@@ -14,67 +37,35 @@ def menu():
     print("10) Cargar mazo equilibrado enemigo")
     print("11) Luchar Jugador VS Jugador")
     print("12) Luchar Jugador VS Bot (Arcade)")
-    print("13) Luchar Jugador VS Bot (liga)")
-
-def cargarCartas(listacartas):
-    fichero = minidom.parse("./XML/Cartas.xml")
-    cartas = fichero.getElementsByTagName("card")
-
-    for carta in cartas:
-        # Lee la informacion del xml y guarda en variables
-        listacarta = []
-        tipo = carta.getAttribute("type")
-        nombre = carta.getElementsByTagName("name")[0]
-        descripcion = carta.getElementsByTagName("description")[0]
-        ataque = carta.getElementsByTagName("attack")[0]
-        defensa = carta.getElementsByTagName("defense")[0]
-        invocacion = carta.getAttribute("summonPoints")
-        # Introduce los datos en una lista
-        listacarta.append(tipo)
-        listacarta.append(nombre.firstChild.data.lstrip().rstrip())
-        listacarta.append(descripcion.firstChild.data.lstrip().rstrip())
-        listacarta.append(ataque.firstChild.data.lstrip().rstrip())
-        listacarta.append(defensa.firstChild.data.lstrip().rstrip())
-        listacarta.append(invocacion)
-        listacartas.append(listacarta) # Introduce la lista en otra lista
-    return listacartas
-
-a = True
-while a != False:
-    menu()
-    try:
-        opc = int(input("Que opcion quieres utilizar? "))
-    except ValueError:
-        print("Opcion invalida, introduce una de las opciones anterior")
-        opc = int(input("Que opcion quieres utilizar? "))
+    print("13) Luchar Jugador VS Bot (liga)")'''
+    opc = int(input("Que opcion quieres utilizar?: "))
 
     if opc == 1:
         cargarCartas(listacartas)
         print(listacartas)
         break
-    elif opc == 2:
+    if opc == 2:
         print("Estas en la opción 2")
-    elif opc == 3:
+    '''
+    if opc == 3:
         print("Estas en la opción 3")
-    elif opc == 4:
+    if opc == 4:
         print("Estas en la opción 4")
-    elif opc == 5:
+    if opc == 5:
         print("Estas en la opción 5")
-    elif opc == 6:
+    if opc == 6:
         print("Estas en la opción 6")
-    elif opc == 7:
+    if opc == 7:
         print("Estas en la opción 7")
-    elif opc == 8:
+    if opc == 8:
         print("Estas en la opción 8")
-    elif opc == 9:
+    if opc == 9:
         print("Estas en la opción 9")
-    elif opc == 10:
+    if opc == 10:
         print("Estas en la opción 10")
-    elif opc == 11:
+    if opc == 11:
         print("Estas en la opción 11")
-    elif opc == 12:
+    if opc == 12:
         print("Estas en la opción 12")
-    elif opc == 13:
-        print("Estas en la opción 13")
-    else:
-        print("Opcion invalida \n")
+    if opc == 13:
+        print("Estas en la opción 13")'''
